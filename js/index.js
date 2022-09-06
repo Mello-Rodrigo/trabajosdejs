c73e86fd25473f6427cc170c19b39b2fbcb6ce1f
mello-rodrigo <mellorodrigo718@gmail.com>
2022-09-06 18:21:01 -0300

    reentrega

--- js/index.js
+++ js/index.js
@@ -1,32 +1,14 @@
 alert("Ingrese el producto que desee llevar, para salir aprete 0")
-let seleccionarProductos = Number(prompt("1-buzo $3000 2-remera $1500 3-jeans $2500 4-zapatillas $6000")
+let seleccionarProductos = Number(prompt("1-buzo $3000 2-remera $1500 3-jeans $2500 4-zapatillas $6000"))
 let seleccionarCantidad;
 let total = 0;
-while (SeleccionarProductos != 0) {
+if(seleccionarProductos > 0 && seleccionarProductos < 5) {
     switch (SeleccionarProductos) {
-        case "1":
-            let SeleccionarCantidad = Number(prompt("El producto seleccionado es el buzo, indique la cantidad. Para salir aprete 0."))
-            total += 3000
-            break;
-        case "2":
-            let SeleccionarCantidad = Number(prompt("El producto seleccionado es la remera, indique la cantidad. Para salir aprete 0."))
-            total += 1500
-            break;
-        case "3":
-            let SeleccionarCantidad = Number(prompt("El producto seleccionado es el jeans, indique la cantidad. Para salir aprete 0."))
-            total += 2500
-            break;
-        case "4":
-            let SeleccionarCantidad = Number(prompt("El producto seleccionado son zapatillas, indique la cantidad. Para salir aprete 0."))
-            total += 6000
-            break;
-
-        default:
+        case 1:
+            seleccionarCantidad = Number(prompt("El producto seleccionado es el buzo, indique la cantidad. Para salir aprete 0."))
+            total += 3000 * seleccionarCantidad;
+            console.Log("el total es de" + total) 
             break;
     }
-    let seleccionarProductos = Number(prompt("1-buzo $3000 2-remera $1500 3-jeans $2500 4-zapatillas $6000")
-}
-
-function suma("el total de la compra es de: " + total) {
-    return "el total de la compra es de: " + total
+    let seleccionarProductos = Number(prompt("1-buzo $3000 2-remera $1500 3-jeans $2500 4-zapatillas $6000"))
 }
