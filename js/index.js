ec4f25719fe5c84412eb8dc4aa3166f09e148663
mello-rodrigo <mellorodrigo718@gmail.com>
2022-09-27 21:21:58 -0300

    entrega

--- js/index.js
+++ js/index.js
@@ -12,6 +12,7 @@ function pay() {
     window.alert(products.join(",\n"))
 }
 
+/*
 const productos = [
     { nombre: "Buzo Nike Sportswear", precio: 5000, img: "img/product-1.jpg" },
     { nombre: "Buzo Nike Air", precio: 7000, img: "img/product-2.jpg" },
@@ -23,7 +24,7 @@ const productos = [
     { nombre: "Remera Nike Sportswear", precio: 4000, img: "img/product-8.jpg" },
     { nombre: "Remera Nike Nsw Tee", precio: 4000, img: "img/product-9.jpg" }
 ]
-
+*/
 
 class Productos {
     constructor(id, nombre, precio, img) {
@@ -35,17 +36,14 @@ class Productos {
 }
 
 products.push(new Productos("1", "Buzo Nike Sportswear", 5000,"img/product-1.jpg" )),
-    (new Productos("2", "Buzo Nike Air", 7000, "img/product-2.jpg")),
-    (new Productos("3", "Buzo con capucha Nike Sportswear Essentials", 7000,"img/product-3.jpg" )),
-    (new Productos("4", "Campera Nike Sportswear Windrunner", 15000,"img/product-4.jpg" )),
-    (new Productos("5", "Campera Nike Tech Fleece", 10000,"img/product-5.jpg" )),
-    (new Productos("6", "Campera Nike Sportswear", 8000,"img/product-6.jpg" )),
-    (new Productos("7", "Remera Nike Roja", 4000, "img/product-7.jpg")),
-    (new Productos("8", "Remera Nike Sportswear", 4000,"img/product-8.jpg" )),
-    (new Productos("9", "Remera Nike Nsw Tee", 4000,"img/product-9.jpg" ))
-
-    console.log(products)
-    products.push
+products.push(new Productos("2", "Buzo Nike Air", 7000, "img/product-2.jpg")),
+products.push(new Productos("3", "Buzo con capucha Nike Sportswear Essentials", 7000,"img/product-3.jpg" )),
+products.push(new Productos("4", "Campera Nike Sportswear Windrunner", 15000,"img/product-4.jpg" )),
+products.push(new Productos("5", "Campera Nike Tech Fleece", 10000,"img/product-5.jpg" )),
+products.push(new Productos("6", "Campera Nike Sportswear", 8000,"img/product-6.jpg" )),
+products.push(new Productos("7", "Remera Nike Roja", 4000, "img/product-7.jpg")),
+products.push(new Productos("8", "Remera Nike Sportswear", 4000,"img/product-8.jpg" )),
+products.push(new Productos("9", "Remera Nike Nsw Tee", 4000,"img/product-9.jpg" ))
 
 const contenedor = document.getElementById("page-content")
 
@@ -54,24 +52,37 @@ const contenedor = document.getElementById("page-content")
     console.log(resultado)
     }
 */
-function renderProductos(array) {
-    array.forEach(producto => {
-        contenedor.innerHTML += `
-                  <div class = "producto-container">
-                       <h3>${producto.nombre}</h3>
-                       <img src=${producto.img} alt=${producto.nombre}>
-                       <button class="button-add">Agregar</button>
-                  </div>
-                  `;
-    }
-    )
+
+const cardShop = (param) => {
+    return ` <div class = "producto-container">
+         <h3>${param.nombre}</h3>
+         <img src=${param.img} alt=${param.nombre}>
+         <button class="button-add" onclick="agregarAlcarrito(${param.id})">Agregar</button>
+    </div>
+    `;
 }
 
-renderProductos(productos)
+function renderProductos(array) {
+   contenedor.innerHTML = array.map(prod => cardShop(prod)).join("")
+}
+
+renderProductos(products)
+
+const cartDiv = (param) => {
+    return ` <div class = "producto-container">
+         <h3>${param.nombre}</h3>
+         <img src=${param.img} alt=${param.nombre}>
+         <button class="button-add" onclick="agregarAlcarrito(${param.id})">Agregar</button>
+    </div>
+    `;
+}
+
+function renderCarrito(array, destino){
+    destino.innerHTML = array.map(producto => cartDiv(producto)).join("")
+}
 
 function agregarAlcarrito(id) {
-    let resultado = productos.find(productos => productos.id == id)
-    console.log(resultado)
-    console.log(carrito)
+    let resultado = products.find(productos => productos.id == id)
+    carrito.push(resultado)
 }
 
